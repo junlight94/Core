0ce7345b2d6f2ecc8b4ce0d38b1b7ee1c77425ed
이준영 <junlight94@gmail.com>
2023-06-27 10:10:49 +0900

    Initial Commit

---
+++ Sources/Design/Design.swift
@@ -0,0 +1,15 @@
+//
+//  Design.swift
+//  
+//
+//  Created by Junyoung Lee on 2023/06/27.
+//
+
+import Foundation
+
+public struct Design {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
